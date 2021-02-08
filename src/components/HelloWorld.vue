377b38da3da30a47c888e7e3e9fa5acd02345dfb
cniklas <hzc9j_yn@posteo.de>
2021-02-08 23:03:32 +0100

    bla

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,23 @@
 <template>
-  <h1>{{ msg }}</h1>
+	<h1>{{ msg }}</h1>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+	<p>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+	</p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+	<button @click="state.count++">count is: {{ state.count }}</button>
+	<p>
+		Edit
+		<code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
 </template>
 
 <script setup>
 import { defineProps, reactive } from 'vue'
 
 defineProps({
-  msg: String
+	msg: String
 })
 
 const state = reactive({ count: 0 })
@@ -25,6 +25,6 @@ const state = reactive({ count: 0 })
 
 <style scoped>
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
