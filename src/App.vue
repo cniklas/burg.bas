cf30b130e0631327af7aafbc16ad930563627c1b
cniklas <hzc9j_yn@posteo.de>
2021-02-20 01:20:34 +0100

    Input processing

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 	<img alt="Vue logo" src="./assets/logo.png" />
 
 	<section>
-		<h3>{{ sceneId }}</h3>
-
 		<article class="story">
 			<template v-for="paragraph in scene.story">
 				<p v-if="typeof paragraph === 'string'" class="text-preline">{{ paragraph }}</p>
@@ -17,13 +15,6 @@
 
 		<div class="actions">
 			<div class="button-wrapper">
-				<button type="button" v-for="command in typeCommands" :disabled="isDisabled(command)" @click="handleCommand(command)" style="font-family:'Courier New',Courier,monospace;font-size:1rem;border-style:dashed">
-					{{ command.text }}
-					{{ command.condition ? `[${command.condition}]` : null }}
-					{{ command.notCondition ? `[NOT ${command.notCondition}]` : null }}
-				</button>
-			</div>
-			<div class="button-wrapper">
 				<button type="button" v-for="command in clickCommands" :disabled="isDisabled(command)" @click="handleCommand(command)">
 					{{ command.text || 'weiter' }}
 					{{ command.condition ? `[${command.condition}]` : null }}
@@ -31,13 +22,22 @@
 				</button>
 			</div>
 
-			<div v-show="scene.hint" class="hint">{{ scene.hint }}</div>
-			<div class="input-wrapper">
-				<input type="text" v-model.trim="typed" class="input" />
+			<div v-show="hint && showHint" class="hint">{{ hint }}</div>
+
+			<div v-show="sceneId !== 'credits'" class="input-wrapper">
+				<input type="text" v-model.trim="typed" class="input" @keyup.enter="handleInput" />
 			</div>
 		</div>
 
 		<div class="debug">
+			<div>{{ sceneId }}</div>
+			<div>
+				<span v-for="command in noClickCommands" class="as-button" :class="{disabled: isDisabled(command)}">
+					{{ command.text }}
+					{{ command.condition ? `[${command.condition}]` : null }}
+					{{ command.notCondition ? `[NOT ${command.notCondition}]` : null }}
+				</span>
+			</div>
 			<pre><code v-for="condition in conditions">{{ `${condition}\n` }}</code></pre>
 		</div>
 	</section>
@@ -51,19 +51,17 @@ let sceneId = ref('start')
 const getSceneById = id => burg.find(scene => scene.id === id)
 const scene = computed(() => getSceneById(sceneId.value))
 
-const typed = ref()
+const typed = ref('')
 
 const clickCommandsList = ['hoch', 'runter', 'links', 'rechts', 'weiter', 'zurück']
 const clickCommands = computed(() => scene.value?.commands?.filter(cmd => clickCommandsList.includes(cmd.text) || cmd.key === 'enter') ?? [])
-const typeCommands = computed(() => scene.value?.commands?.filter(cmd => !clickCommandsList.includes(cmd.text) && cmd.key !== 'enter') ?? [])
+const noClickCommands = computed(() => scene.value?.commands?.filter(cmd => !clickCommandsList.includes(cmd.text) && cmd.key !== 'enter') ?? [])
 
 const conditions = ref([])
-// const findCondition = (term) => {
-// 	return conditions.value.find(c => c === term)
-// }
-const hasCondition = (term) => {
-	return conditions.value.includes(term)
-}
+const hasCondition = term => conditions.value.includes(term)
+
+const hint = ref('')
+const showHint = ref(false)
 
 const randomBattle = () => {
 	const rnd = Math.floor(Math.random() * Math.floor(3))
@@ -94,21 +92,20 @@ const handleAction = command => {
 		resetGame()
 	}
 
+	showHint.value = false
+	hint.value = ''
 	typed.value = ''
 	sceneId.value = command.action
+	document.querySelector('.input').focus()
 }
 
 const handleMessage = command => {
-	alert(command.message)
+	hint.value = command.message
+	showHint.value = true
 }
 
 const handleCommand = command => {
-	if (command.message) {
-		handleMessage(command)
-		return
-	}
-
-	handleAction(command)
+	command.message ? handleMessage(command) : handleAction(command)
 }
 
 const isDisabled = ({ condition, notCondition }) => {
@@ -124,6 +121,37 @@ const isDisabled = ({ condition, notCondition }) => {
 
 	return false
 }
+
+const cleanInput = (string) => {
+	const regex = /[!"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{\|}~]/g;
+	return string
+		.replaceAll(regex, '')
+		.toLowerCase()
+		.split(' ')
+		.filter(word => word.length && !['der', 'die', 'das', 'den', 'und'].includes(word))
+		.join(' ')
+}
+
+const handleInput = () => {
+	const input = cleanInput(typed.value)
+
+	let command
+	if (input === 'weiter') {
+		command = scene.value.commands?.find(cmd => cmd.key === 'enter')
+	}
+
+	if (command === undefined) {
+		command = scene.value.commands?.find(cmd => !isDisabled(cmd) && cmd.text.toLowerCase() === input)
+	}
+
+	if (command === undefined) {
+		hint.value = scene.value.hint || ''
+		showHint.value = true
+		return
+	}
+
+	handleCommand(command)
+}
 </script>
 
 <style>
@@ -132,9 +160,18 @@ html {
 	color: #cbd5e0;
 }
 
+pre:empty {
+	margin: 0;
+}
+
+input {
+	font-family: inherit;
+	font-size: inherit;
+	line-height: inherit;
+}
+
 button {
 	background-color: #1a202c;
-	color: #a0aec0;
 	color: cadetblue;
 	font-family: inherit;
 	font-size: inherit;
@@ -146,30 +183,22 @@ button {
 	user-select: none;
 }
 
-/* button:focus, */
-button:not(:disabled):hover {
-	color: #e2e8f0;
-}
-
 button:disabled {
 	opacity: .4;
 	cursor: initial;
 }
 
-.debug {
-	font-family: 'Courier New', Courier, monospace;
-	outline: 2px dashed deeppink;
+/* button:focus, */
+button:not(:disabled):hover {
+	color: #e2e8f0;
 }
 
 #app {
-	/* font-family: Avenir, Helvetica, Arial, sans-serif; */
 	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	font-family: Menlo, 'DejaVu Sans Mono', 'Lucida Console', monospace;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	/* color: #2c3e50; */
-	margin-top: 60px;
-
 	margin: 60px auto 0;
 	max-width: 720px;
 }
@@ -179,14 +208,10 @@ button:disabled {
 	margin: 2rem 0;
 }
 
-.button-wrapper,
 .hint,
+.button-wrapper,
 .input-wrapper {
 	margin: 1rem 0;
-}
-
-.hint {
-	font-style: italic;
 }
 
 .story {
@@ -199,6 +224,32 @@ button:disabled {
 }
 
 .story .disabled {
+	opacity: .35;
+}
+
+.hint {
+	font-style: italic;
+}
+
+.debug {
+	font-family: 'Courier New', Courier, monospace;
+	border: 2px dashed deeppink;
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	max-width: 720px;
+}
+
+.debug > * {
+	margin: .75rem 0;
+}
+
+.as-button {
+	display: inline-block;
+	margin: 0 .5rem;
+}
+
+.as-button .disabled {
 	opacity: .35;
 }
 </style>
