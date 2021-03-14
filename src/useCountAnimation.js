47afd894ae6962ec7c129080494e1979eb6180d3
cniklas <hzc9j_yn@posteo.de>
2021-03-14 21:50:47 +0100

    bla

--- src/useCountAnimation.js
+++ src/useCountAnimation.js
@@ -4,35 +4,35 @@ export default function() {
 	// Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
 	const _frameDuration = 1000 / 60
 	// Use that to calculate how many frames we need to complete the animation
-	const _totalFrames = Math.round( _animationDuration / _frameDuration )
+	const _totalFrames = Math.round(_animationDuration / _frameDuration)
 	// An ease-out function that slows the count as it progresses
-	const _easeOutQuad = t => t * ( 2 - t )
+	const _easeOutQuad = t => t * (2 - t)
 
-	const animateCount = (el, amount, add = true) => {
-		const currentValue = el.value
+	const animateCount = (el, changeBy, add = true) => {
+		const oldValue = el.value
+		let number = el.value
 		let frame = 0
-		const countTo = amount
 
 		// Start the animation running 60 times per second
-		const counter = setInterval( () => {
+		const counter = setInterval(() => {
 			frame++
 			// Calculate our progress as a value between 0 and 1
 			// Pass that value to our easing function to get our progress on a curve
-			const progress = _easeOutQuad( frame / _totalFrames )
+			const progress = _easeOutQuad(frame / _totalFrames)
 			// Use the progress value to calculate the current count
-			const currentCount = Math.round( countTo * progress )
+			const currentCount = Math.round(changeBy * progress)
 
 			// If the current count has changed, update the element
-			if ( amount !== currentCount ) {
-				amount = currentCount
-				el.value = add ? currentValue + amount : currentValue - amount
+			if (number !== currentCount) {
+				number = currentCount
+				el.value = add ? oldValue + number : oldValue - number
 			}
 
 			// If we’ve reached our last frame, stop the animation
-			if ( frame === _totalFrames ) {
-				clearInterval( counter )
+			if (frame === _totalFrames) {
+				clearInterval(counter)
 			}
-		}, _frameDuration )
+		}, _frameDuration)
 	}
 
 	return {
