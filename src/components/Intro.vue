f0d8521e591dcdd9d7f6d4fcb778398325c5d004
cniklas <hzc9j_yn@posteo.de>
2021-04-03 22:04:48 +0200

    Better random names. Timeout

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-<pre class="ascii-text">
+<!-- <pre class="ascii-text">
 88                                                 88
 88                                                 88
 88                                                 88
@@ -10,8 +10,7 @@
 8Y"Ybbd8"'   `"YbbdP'Y8 88          `"YbbdP"Y8 888 8Y"Ybbd8"'  `"8bbdP"Y8 `"YbbdP"'
                                     aa,    ,88
                                      "Y8bbdP"
-</pre>
-
+</pre> -->
 	<section class="actions">
 		<div class="input-wrapper">
 			<input type="text" v-model.trim="userName" ref="input" class="input" placeholder="Gib einen Namen ein oder drücke ENTER" @click.stop @keyup.enter="createName" />
@@ -38,32 +37,138 @@ const { loadMusic } = useHowler
 const userName = ref('')
 const showButton = computed(() => userName.value.length >= 2)
 
-const firstNames = [
-	'Basil',
-	'Herbert',
-	'Winston',
-	'Fredrick',
-	'Grumpf',
-	'Wilhelm',
-	'Theodore',
-	'Greasus'
+// const firstNames = [
+// 	'Basil',
+// 	'Herbert',
+// 	'Winston',
+// 	'Fredrick',
+// 	'Grumpf',
+// 	'Wilhelm',
+// 	'Theodore',
+// 	'Greasus'
+// ]
+// const lastNames = [
+// 	'Berrybrook',
+// 	'Deathrump',
+// 	'Wellington',
+// 	'Bloodlord',
+// 	'Firerump',
+// 	'Flamebeard',
+// 	'the Flatulant IV',
+// 	'Thornheart',
+// 	'Ragefist',
+// 	'Goldtooth',
+// 	'Guillemaere'
+// ]
+const simCityNames = [
+	// 'Franz Ausbeuth',
+	// 'Holger Ausbunt',
+	// 'Wastl Ballwix',
+	// 'Michel Bänger',
+	'Xaver Birnenschmaus',
+	// 'Olaf Bitterbiß',
+	// 'Luis Blunzensee',
+	'Louis Blunzensee',
+	// 'Engelhart Bontz',
+	// 'Gerd Böswig',
+	'Frieder Brausewitz',
+	// 'Peer Brunsen',
+	// 'Hans Bullmann',
+	// 'Gustav Bürzel',
+	// 'Freddi Dollboehr',
+	// 'Kaspar Dotterblum',
+	'Klaus Drösenklump',
+	// 'Pit Dummke',
+	// 'Oskar Dünkelhofer',
+	// 'Rudi Durchblick',
+	'Ernst Eberschneck',
+	'Edgar Ellenpohl',
+	// 'Paul Ersazki',
+	// 'Erwin Fasel',
+	// 'Berthold Fatzke',
+	// 'Freddi Fiesewetter',
+	// 'Reinhard Fitzelbräu',
+	// 'Axel Flachmann',
+	'Engelbert Flusenberg',
+	// 'Sebastian Gallgentritt',
+	// 'Egon Gerngroß',
+	// 'Igor Grobloch',
+	// 'Waldemar Grünspecht',
+	// 'Helmut Haslam',
+	// 'Willi Haubald',
+	'Valentin Haxenpfeffer',
+	// 'Wenzeslaus Hitzig',
+	// 'Dagobert Holzweg',
+	// 'Rudi Karmichel',
+	// 'Jockel Katzmauski',
+	// 'Till Klecks',
+	// 'Wladimir Klugscheit',
+	'Karl Knall',
+	// 'Wolf Knüppeldieck',
+	// 'Helmut Krauth',
+	// 'Oskar Lallbeck',
+	// 'Ede Langbeen',
+	// 'Kalle Langöhr',
+	// 'Fritz Lausmann',
+	// 'Eddie Metzger',
+	'Wolf Nasenstieb',
+	// 'Detlef Onemichel',
+	// 'Wendelin Pausback',
+	// 'Adalgeiß Pikemann',
+	'Samuel Plopps',
+	'Markus Rinderbrüh',
+	// 'Jens Rotlöffel',
+	// 'Ödon Ruckmair',
+	// 'Albert Schleibich',
+	'Martin Schlürfnich',
+	// 'Peter Schnutzelwieser',
+	// 'Dagobert Schuhlöffel',
+	'Tobias Seitenstich',
+	// 'Eduard Spürsinn',
+	// 'Pluto Stachelski',
+	'Gottlieb Stegreif',
+	'Botho Sternschnupf',
+	// 'Robbie Sterz',
+	// 'Piet Stormke',
+	// 'Franz Strammbein',
+	// 'Heiner Stünkel',
+	// 'Erich Treppwitz',
+	// 'Philidor Trugschlüß',
+	'Ürdü Tüdelüt',
+	// 'Dirk Tümper',
+	// 'Theo Unbilt',
+	// 'Eddie Untenraus',
+	'Golo Unterzwerch',
+	// 'Arnulf von Achsenbruch',
+	'Arnulf von Wannensteg',
+	'Hanno von Büsenstreich',
+	// 'Blasius Waldschrag',
+	// 'Hugo Wamp',
+	// 'Fritz Wangengrün',
+	// 'Armin Wannensteg',
+	// 'Iwan Wasndeppski',
+	// 'Emil Watts',
+	// 'Holger Weißnitz',
+	'Leo Wellblech',
+	// 'Konrad Wexelbald',
+	// 'Jörg Wiedeborst',
+	// 'Gustav Witzbald',
+	'Wilhelm Wohlgemuth',
+	// 'Rudolf Wolf',
+	// 'Florian Wolkenbruch',
+	'Trollo Zahnlos',
+	// 'Andreas Zahnstein',
+	// 'Karol Ersatzki'
 ]
-const lastNames = [
-	'Berrybrook',
-	'Deathrump',
-	'Wellington',
-	'Bloodlord',
-	'Firerump',
-	'Flamebeard',
-	'the Flatulant IV',
-	'Thornheart',
-	'Ragefist',
-	'Goldtooth',
-	'Guillemaere'
-]
-const randomize = (store) => store[ Math.floor(Math.random() * store.length) ]
+let names = []
+// const randomize = (store) => store[ Math.floor(Math.random() * store.length) ]
+const randomSplice = (store) => store.splice( Math.floor(Math.random() * store.length), 1 ).shift()
 const createName = () => {
-	userName.value = `${randomize(firstNames)} ${randomize(lastNames)}`
+	// userName.value = `${randomize(firstNames)} ${randomize(lastNames)}`
+	if (!names.length) {
+		names = [...simCityNames]
+	}
+	userName.value = randomSplice(names)
 }
 
 const input = ref(null)
