2adf0289cb25cdf1dfc8891808436c5ef12bee59
Christian Niklas <christian.niklas@outletcity.com>
2021-05-31 15:33:10 +0200

    write modal text

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,33 +7,34 @@
 				<h2 class="headline text-center mb-6">Über dieses Spiel</h2>
 
 				<article class="story">
-					<p>Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen.</p>
-
-					<p>Manchmal benutzt man Worte wie Hamburgefonts, Rafgenduks oder Handgloves, um Schriften zu testen. Manchmal Sätze, die alle Buchstaben des Alphabets enthalten - man nennt diese Sätze »Pangrams«.</p>
-
-					<p>Sehr bekannt ist dieser: The quick brown fox jumps over the lazy old dog.</p>
-
-					<p>Oft werden in Typoblindtexte auch fremdsprachige Satzteile eingebaut (AVAIL® and Wefox™ are testing aussi la Kerning), um die Wirkung in anderen Sprachen zu testen. In Lateinisch sieht zum Beispiel fast jede Schrift gut aus.</p>
-
-					<p>Quod erat demonstrandum. Seit 1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach TypoGb. 204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden.</p>
-
-					<p>Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft. Genauso wichtig in sind mittlerweile auch Âçcèñtë, die in neueren Schriften aber fast immer enthalten sind. Ein wichtiges aber schwierig zu integrierendes Feld sind OpenType-Funktionalitäten.</p>
-
-					<p>Je nach Software und Voreinstellungen können eingebaute Kapitälchen, Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt werden.</p>
-
-					<p>Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen.</p>
-
-					<p>Manchmal benutzt man Worte wie Hamburgefonts, Rafgenduks oder Handgloves, um Schriften zu testen. Manchmal Sätze, die alle Buchstaben des Alphabets enthalten - man nennt diese Sätze »Pangrams«.</p>
-
-					<p>Sehr bekannt ist dieser: The quick brown fox jumps over the lazy old dog.</p>
-
-					<p>Oft werden in Typoblindtexte auch fremdsprachige Satzteile eingebaut (AVAIL® and Wefox™ are testing aussi la Kerning), um die Wirkung in anderen Sprachen zu testen. In Lateinisch sieht zum Beispiel fast jede Schrift gut aus.</p>
-
-					<p>Quod erat demonstrandum. Seit 1975 fehlen in den meisten Testtexten die Zahlen, weswegen nach TypoGb. 204 § ab dem Jahr 2034 Zahlen in 86 der Texte zur Pflicht werden.</p>
-
-					<p>Nichteinhaltung wird mit bis zu 245 € oder 368 $ bestraft. Genauso wichtig in sind mittlerweile auch Âçcèñtë, die in neueren Schriften aber fast immer enthalten sind. Ein wichtiges aber schwierig zu integrierendes Feld sind OpenType-Funktionalitäten.</p>
-
-					<p>Je nach Software und Voreinstellungen können eingebaute Kapitälchen, Kerning oder Ligaturen (sehr pfiffig) nicht richtig dargestellt werden.</p>
+					<p>
+						„Die Burg“ ist ein Textadventure Game à la <a href="https://www.c64-wiki.de/wiki/Zauberschlo%C3%9F" class="underline" rel="noopener">Zauberschloß</a>. Ich habe es 1996 als 17jähriger programmiert, nachdem mich mein jüngerer Bruder mit <a href="https://de.wikipedia.org/wiki/BASIC" class="underline" rel="noopener">BASIC</a> angesteckt hatte.
+					</p>
+					<p>
+						2021 habe ich das komplette Spiel in JavaScript nachgebaut, so dass es nun im Browser gespielt werden kann.
+						Der Aufbau des Spiels ist noch derselbe wie damals; nur die Texte habe ich überarbeitet und aufgefrischt.
+					</p>
+					<p>
+						Die Musikstücke für das Intro und den Endkampf habe ich um mehr als die Hälfte gekürzt. Hier sind die Originale in voller Länge zu hören:
+					</p>
+					<ul>
+						<li class="flex items-center">
+							<audio src="audio/intro-2-o.mp3" controls></audio> <span class="ml-4">Intro</span>
+						</li>
+						<li class="flex items-center">
+							<audio src="audio/intro-1.mp3" controls></audio> <span class="ml-4">alternatives Intro</span>
+						</li>
+						<li class="flex items-center">
+							<audio src="audio/battle-o.mp3" controls></audio> <span class="ml-4">Battle</span>
+						</li>
+					</ul>
+					<p>
+						Der ursprüngliche Endkampf war eine simple Wahrscheinlichkeitsrechnung, bei der der Spieler mit einer Chance von 2:3 gewonnen hat.<br>
+						Nun wird diese alles entscheidende Szene in Form eines etwas schrägen Protokolls wiedergegeben und die Aussicht auf Sieg oder Niederlage von vielen Zufällen abhängig gemacht.
+					</p>
+					<p>
+						Die verrückten Namensvorschläge für den Spieler stammen aus <a href="https://en.wikipedia.org/wiki/SimCity_2000" class="underline" rel="noopener">SimCity 2000</a>.
+					</p>
 				</article>
 			</div>
 		</section>
@@ -62,26 +63,20 @@ const toggleModal = () => {
 <style lang="postcss" scoped>
 .modal-button {
 	@apply fixed top-4 left-1/2 z-50 hidden lg:block p-0 w-8 h-8 border-2 rounded-full;
-	/* color: hsl(182, 25%, 64%); */
 	font-family: 'Courier New', Courier, monospace;
 	font-size: 1.5rem;
 	line-height: calc(2rem - 4px);
 	box-shadow: none;
-	/* transform: translateX(calc(-50vw - 50% + 2rem)); */
-	transform: translateX(calc(-512px - 50% + 2rem));
+	transform: translateX(calc(-512px - 50% + 2rem)); /* translateX(calc(-50vw - 50% + 2rem)) */
 	transition: color 160ms, transform var(--modal-transition-duration);
 }
-
-/* .modal-button:hover {
-	color: hsl(182, 35%, 64%);
-} */
 
 .modal-button.is-active {
 	transform: translateX(-50%);
 }
 
 .modal-wrapper {
-	@apply fixed flex justify-center items-center w-full h-full top-0 left-0 invisible z-40; /* py-16 */
+	@apply fixed flex justify-center items-center w-full h-full top-0 left-0 invisible z-40;
 	backface-visibility: hidden;
 }
 
@@ -93,7 +88,7 @@ const toggleModal = () => {
 .modal {
 	@apply px-4 opacity-0 overflow-y-auto;
 	max-width: 44rem;
-	max-height: 61.8%;
+	max-height: 70%; /* 61.8% */
 	transform: scale(0.8);
 	transition: all var(--modal-transition-duration);
 }
@@ -118,5 +113,13 @@ const toggleModal = () => {
 .is-visible + .modal-overlay {
 	@apply opacity-100 visible;
 	background-color: var(--bg-color);
+}
+
+.story ul {
+	margin-bottom: calc(var(--global-line-height) * 1rem);
+}
+
+.story li:nth-last-child(n+2) {
+	margin-bottom: calc(var(--global-line-height) * 0.5rem);
 }
 </style>
